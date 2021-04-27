0e45f04b7bd7ea0b053331d16cb4a65138592ba7
Eshika Khandelwal <eshika.k@research.iiit.ac.in>
2021-04-27 15:27:16 +0530

    Initialised and print board

--- checkers.c
+++ checkers.c
@@ -1,3 +1,90 @@
 #include "checkers.h"
 
+void initBoard(checkersGrid Board[][SIZE])
+{
+    //Create empty board
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
 
+            Board[i][j].state = EMPTY;
+            (Board[i][j].checkers).colour = NOCOLOUR;
+            (Board[i][j].checkers).type = NOPEICE;
+        }
+    }
+
+    //place the inital pieces
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < SIZE; j = j + 2)
+        {
+            if (i == 1)
+            {
+                Board[i][j + 1].state = FULL;
+                (Board[i][j + 1].checkers).colour = RED;
+                (Board[i][j + 1].checkers).type = NORMAL;
+
+                Board[SIZE - 1 - i][j + 1].state = FULL;
+                (Board[SIZE - 1 - i][j + 1].checkers).colour = BLUE;
+                (Board[SIZE - 1 - i][j + 1].checkers).type = NORMAL;
+            }
+            else
+            {
+                Board[i][j].state = FULL;
+                (Board[i][j].checkers).colour = RED;
+                (Board[i][j].checkers).type = NORMAL;
+
+                Board[SIZE - 1 - i][j].state = FULL;
+                (Board[SIZE - 1 - i][j].checkers).colour = BLUE;
+                (Board[SIZE - 1 - i][j].checkers).type = NORMAL;
+            }
+        }
+    }
+}
+
+void printBoard(checkersGrid Board[][SIZE])
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+
+        for (int k = 0; k < SIZE + 1; k++)
+        {
+            if (k == 0)
+                printf("   ");
+            else
+                printf("----");
+        }
+        printf("-");
+        printf("\n");
+
+        printf("%d  |", i + 1);
+        for (int j = 0; j < SIZE; j++)
+        {
+
+            if ((Board[i][j].state == FULL) && ((Board[i][j].checkers).colour == RED) && ((Board[i][j].checkers).type == NORMAL))
+                printf(" %c |", 'X');
+            else if ((Board[i][j].state == FULL) && ((Board[i][j].checkers).colour == BLUE) && ((Board[i][j].checkers).type == NORMAL))
+                printf(" %c |", 'O');
+            else
+                printf("   |");
+        }
+        printf("\n");
+    }
+    for (int k = 0; k < SIZE + 1; k++)
+    {
+        if (k == 0)
+            printf("   ");
+        else
+            printf("----");
+    }
+    printf("\n");
+    printf("   ");
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("  %c ", 'A' + i);
+    }
+    printf("\n");
+}
--- constants.h
+++ constants.h
@@ -4,8 +4,8 @@
 // size of checkers
 #define SIZE 8
 
-// colors 
-#define WHITE -1
+// colors
+#define NOCOLOUR -1
 #define BLUE 0
 #define RED 1
 
@@ -13,18 +13,30 @@
 #define FULL 2
 #define EMPTY -2
 
+// type of peice
+#define NORMAL 3
+#define KING 4
+#define NOPEICE -3
 
-struct checkersGrid{
-    int isFull;  //empty or full
-    int color;   //red, blue or white
-};
+//structures for board and the peices
 
+typedef struct coordinates
+{
+    int x; // board starts from 1, hence input - 1 will be stored.
+    int y; // coordinate y is an aplhabet so the ascii - 'A' can be stored.
+} coordinates;
 
-struct coordinates{
-    int x;
-    int y;
-};
+typedef struct peice
+{
+    int type; // whether the peice is a king or a normal piece. No piece if that coordinate has no peice on the b
+    int colour; //colour of the peice, will be no colour if no peice exists on the location
+} peice;
 
+typedef struct checkersGrid
+{
+    int state; //empty or full
+    peice checkers; // peice assosciated with that position in the grid;
+} checkersGrid;
 
 
 #endif
