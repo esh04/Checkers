5ce263a1863aa0b856d9a847fe05bd71a98985e5
Lavisha Bhambri <bhambrilavisha@gmail.com>
2021-05-05 15:21:59 +0530

    Undo function added

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include "checkers.h"
+#include "stack.h"
+
+StackContents stack[10000];
 
 int main()
 {
@@ -30,12 +33,26 @@ int main()
         y1 = toupper(y1);
         y2 = toupper(y2);
 
+<<<<<<< Updated upstream
         // Converting to integer values
         start.y = y1 - 'A';
         final.y = y2 - 'A';
 
         int ans = movements(CheckerBoard, turn, start, final);
         printf("\n\n");
+=======
+            // Converting to integer values
+            start.y = y1 - 'A';
+            final.y = y2 - 'A';
+            int capture = 1;
+
+            if(captures(CheckerBoard, turn, start, final) == 0){
+                capture = 0;
+            }
+
+            ans = captures(CheckerBoard, turn, start, final) || movements(CheckerBoard, turn, start, final);
+            printf("\n\n");
+>>>>>>> Stashed changes
 
         if (ans == 0)
         {
@@ -50,8 +67,48 @@ int main()
                 turn = 'O';
             }
             else
+<<<<<<< Updated upstream
             { // Player- X turn
                 turn = 'X';
+=======
+            {   
+                printBoard(CheckerBoard);
+                // If Valid move push into stack everytime
+                StackContents StackValues;
+                StackValues.start = start;
+                StackValues.final = final;
+                StackValues.turn  = turn;
+                push(stack, StackValues);
+
+                // Asking for Undo Option from user
+                printf("Press 1 if you want to Undo, else press any other key!\n");
+                int a;
+                scanf("%d",&a);
+                if(a==1){
+                    int moves;
+                    printf("Enter no. of moves you want to Undo\n");
+                    scanf("%d", &moves);
+
+                    // Do Undo 
+                    int undoAns = undo(CheckerBoard, stack, moves, capture);
+                    if(undoAns==0){
+                        printf("The no. of moves were Invalid");
+                    }
+                }
+
+                
+                // Valid move
+                if (turn == 'X')
+                {  
+                    // Player- O turn
+                    turn = 'O';
+                }
+                else
+                { // Player- X turn
+                    turn = 'X';
+                }
+                
+>>>>>>> Stashed changes
             }
         }
     }
