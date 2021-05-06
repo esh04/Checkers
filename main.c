966626f7a0c7e490f9f962ca35b35ef554d1fda7
Eshika Khandelwal <eshika.k@research.iiit.ac.in>
2021-05-07 00:25:12 +0530

    fixed bug in undo function

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main()
     int ans = 1;
     char turn = 'O';
     char ch;
-    asciiArt();
+    introduction();
     if (scanf("%c", &ch))
         while (1)
         {
@@ -46,11 +46,9 @@ int main()
             final.y = y2 - 'A';
 
             int capture = captures(CheckerBoard, turn, start, final);
-            
-            ans =  movements(CheckerBoard, turn, start, final) || capture;
+
+            ans = movements(CheckerBoard, turn, start, final) || capture;
             printf("\n\n");
-
-
 
             if (ans == 0)
             {
@@ -58,40 +56,47 @@ int main()
                 printf("\n      Invalid Move, Try Again!\n");
             }
             else
-            {   
+            {
                 printBoard(CheckerBoard);
                 // If Valid move push into stack everytime
                 StackContents StackValues;
                 StackValues.start = start;
                 StackValues.final = final;
-                StackValues.turn  = turn;
+                StackValues.turn = turn;
                 push(stack, StackValues);
 
                 // Asking for Undo Option from user
                 printf("Press 1 if you want to Undo, else press any other key!\n");
                 int a;
-                scanf("%d",&a);
-                if(a==1){
+                scanf("%d", &a);
+                if (a == 1)
+                {
                     int moves;
                     printf("Enter no. of moves you want to Undo\n");
                     scanf("%d", &moves);
 
-                    // Do Undo 
+                    // Do Undo
                     int undoAns = undo(CheckerBoard, stack, moves, capture);
-                    if(turn=='X'){
-                        turn = 'O';
-                    }else{
-                        turn = 'X';
+                    if (moves % 2 == 1)
+                    {
+                        if (turn == 'X')
+                        {
+                            turn = 'O';
+                        }
+                        else
+                        {
+                            turn = 'X';
+                        }
                     }
-                    if(undoAns==0){
+                    if (undoAns == 0)
+                    {
                         printf("The no. of moves were Invalid");
                     }
                 }
 
-                
                 // Valid move
                 if (turn == 'X')
-                {  
+                {
                     // Player- O turn
                     turn = 'O';
                 }
@@ -99,7 +104,6 @@ int main()
                 { // Player- X turn
                     turn = 'X';
                 }
-
             }
         }
     return 0;
